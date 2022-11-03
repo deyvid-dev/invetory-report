bbe9c67ed88ba2e79e2344dbf5c0044ec71bd2c4
deyvid-dev <deyvidscm@hotmail.com>
2022-11-03 01:51:17 -0300

    req-02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "produto",
+        "empresa",
+        "2022-01-01",
+        "2023-01-01",
+        "123654",
+        "instru_armazem"
+    )
+
+    assert produto.__str__() == "O produto produto fabricado em 2022-01-01" \
+        " por empresa com validade até 2023-01-01" \
+        " precisa ser armazenado instru_armazem."
