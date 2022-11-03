0eb5e849fbeedc6f6fbcc83188732e310688ccf3
deyvid-dev <deyvidscm@hotmail.com>
2022-11-03 00:23:42 -0300

    req-01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "produto",
+        "empresa",
+        "2022-01-01",
+        "2023-01-01",
+        "123654",
+        "instru_armazem"
+    )
+
+    assert produto.id == 1
+    assert produto.nome_do_produto == "produto"
+    assert produto.nome_da_empresa == "empresa"
+    assert produto.data_de_fabricacao == "2022-01-01"
+    assert produto.data_de_validade == "2023-01-01"
+    assert produto.numero_de_serie == "123654"
+    assert produto.instrucoes_de_armazenamento == "instru_armazem"
