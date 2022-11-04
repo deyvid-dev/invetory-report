4938a9fe68b72d8b30beef3ba239b15785c277e6
deyvid-dev <deyvidscm@hotmail.com>
2022-11-04 00:36:59 -0300

    req-06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,19 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, type):
-        with open(path, encoding="utf-8") as file:
-            lista = list(csv.DictReader(file))
+    def import_data(cls, path: str, type):
+        if path.endswith(".csv"):
+            with open(path, encoding="utf-8") as file:
+                lista = list(csv.DictReader(file))
+
+        if path.endswith(".json"):
+            with open(path) as file:
+                lista = json.load(file)
 
         if type == "simples":
             return SimpleReport.generate(lista)
