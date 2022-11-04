760ecfd970531fb801230d98ea7a5048373cd659
deyvid-dev <deyvidscm@hotmail.com>
2022-11-04 00:47:22 -0300

    req-07

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -14,6 +15,10 @@ class Inventory:
         if path.endswith(".json"):
             with open(path) as file:
                 lista = json.load(file)
+
+        if path.endswith(".xml"):
+            with open(path) as file:
+                lista = xmltodict.parse(file.read())["dataset"]["record"]
 
         if type == "simples":
             return SimpleReport.generate(lista)
